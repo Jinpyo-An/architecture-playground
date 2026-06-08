fd493ccea9c34a82df0119507f074ced2425b077
Jinpyo An <dkswlsvy3312@gmail.com>
2026-06-08 15:08:08 +0900

    msa swagger, 인가 구현

--- src/main/java/com/example/exercise/config/SwaggerConfig.java
+++ src/main/java/com/example/exercise/config/SwaggerConfig.java
@@ -1,5 +1,9 @@
 package com.example.exercise.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,17 +11,29 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
 
     @Value("${api.init}")
     private String apiInit;
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url("/exercise-service")))
                 .info(new Info()
                         .title("Backend API")
                         .description("Backend Swagger API Docs")
-                        .version(apiInit));
+                        .version(apiInit))
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"));
     }
 }
+
