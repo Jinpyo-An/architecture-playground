93d72fd36eda07248baf547ff3a1c02bc75a8bbc
Jinpyo An <dkswlsvy3312@gmail.com>
2026-06-09 09:48:53 +0900

    컨디션 제거

--- src/main/java/com/example/exercise/payment/infrastructure/event/PaymentIntegrationEventHandler.java
+++ src/main/java/com/example/exercise/payment/infrastructure/event/PaymentIntegrationEventHandler.java
@@ -6,6 +6,7 @@ import com.example.exercise.payment.application.event.PaymentConfirmedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -13,6 +14,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
+@ConditionalOnProperty(prefix = "kafka", name = "enabled", havingValue = "true")
 @RequiredArgsConstructor
 public class PaymentIntegrationEventHandler {
     @Value("order-service")
